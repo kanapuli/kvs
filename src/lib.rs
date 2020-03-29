e346a5bc73870985be69ba06963c528be914eb56
kanapuli <athavankanapuli@gmail.com>
2020-03-29 18:28:07 +0530

    Implement core Kvs methods
    Add the following methods with unimplemented!() body
    new(), get(), set(), remove()

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,25 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+//KvStore stores the key and values in memory
+pub struct KvStore {}
+
+impl KvStore {
+    #[warn(dead_code)]
+    // creates a new instance of KvStore
+    pub fn new() -> Self {
+        unimplemented!();
+    }
+
+    //set creates a new Key Value pair
+    pub fn set(&mut self, _key: String, _value: String) {
+        unimplemented!();
+    }
+
+    //get gets the value for a key
+    pub fn get(&self, _key: String) -> Option<String> {
+        unimplemented!();
+    }
+
+    //remove remove a key and a value from the in-memory store
+    pub fn remove(&mut self, _key: String) {
+        unimplemented!();
     }
 }
