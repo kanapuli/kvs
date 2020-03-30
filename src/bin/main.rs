e5a4204a8c71bdfc1bb5ef04ff4a4d66b2211ee4
kanapuli <athavankanapuli@gmail.com>
2020-03-30 07:08:03 +0530

    Parse get and set commands from the command line arguments
    use clap args to parse get and set arguments

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,23 @@
+extern crate clap;
+
+use clap::{App, Arg};
+
 fn main() {
-    println!("{}", "kvs is a in memory key-value store");
+    let app = App::new("Kvs - InMemory Key value store")
+        .version("1.0")
+        .author("kanapuli")
+        .about("A superfast InMemory key value store")
+        .arg(
+            Arg::with_name("get")
+                .long("get")
+                .help("get - retrieves a value for the given key")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("set")
+                .long("set")
+                .help("set - sets the value for a given key")
+                .takes_value(true),
+        )
+        .get_matches();
 }
