aa2b4d42cdbafed7b9f05e7bd550ce6f0c6f039f
kanapuli <athavankanapuli@gmail.com>
2020-03-31 21:08:34 +0530

    Get the args using clap subcommands
    Remove Args implementation. Use Clap SubCommands instead. SubCommands
    helps to parse values for get, set and rm commands

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,23 +1,39 @@
 extern crate clap;
 
-use clap::{App, Arg};
+use clap::{App, Arg, SubCommand};
 
 fn main() {
     let app = App::new("Kvs - InMemory Key value store")
         .version("1.0")
         .author("kanapuli")
         .about("A superfast InMemory key value store")
-        .arg(
-            Arg::with_name("get")
-                .long("get")
-                .help("get - retrieves a value for the given key")
-                .takes_value(true),
+        .subcommand(
+            SubCommand::with_name("get")
+                .about("get - retrieves a value for the given key")
+                .arg(
+                    Arg::with_name("KEY")
+                        .help("string value of the key")
+                        .required(true),
+                ),
         )
-        .arg(
-            Arg::with_name("set")
-                .long("set")
-                .help("set - sets the value for a given key")
-                .takes_value(true),
+        .subcommand(
+            SubCommand::with_name("rm")
+                .about("rm - Deletes the key and its value from the kvs store")
+                .arg(
+                    Arg::with_name("KEY")
+                        .help("string value of the key")
+                        .required(true),
+                ),
+        )
+        .subcommand(
+            SubCommand::with_name("set")
+                .about("set - stores a value for the given key")
+                .arg(
+                    Arg::with_name("KEY")
+                        .help("string value of the key")
+                        .required(true),
+                )
+                .arg(Arg::with_name("VALUE").help("string value").required(true)),
         )
         .get_matches();
 }
