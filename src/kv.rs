9423dcddd85b4778043d5253b6186809394e2297
kanapuli <athavankanapuli@gmail.com>
2020-05-10 20:35:49 +0530

    Comment all existing implementations and add KvStore fields for log storage

--- src/kv.rs
+++ src/kv.rs
@@ -1,39 +1,52 @@
+use std::path::PathBuf;
+use std::fs::File;
 use std::collections::HashMap;
+use std::io::{BufReader,BufWriter, Read, Seek, Write};
 
 //KvStore stores the key and values in memory
 pub struct KvStore {
-    map: HashMap<String, String>,
+  //directory for the log to store
+  path: PathBuf,
+  readers: HashMap<u64, BufReaderWithPos<File>>,
+  //writer of the current log
+  writer: BufWriterWithPos<File>, 
+
 }
 
-impl KvStore {
-    #[warn(dead_code)]
-    // creates a new instance of KvStore
-    pub fn new() -> Self {
-        KvStore {
-            map: HashMap::new(),
-        }
-    }
-
-    //set creates a new Key Value pair
-    pub fn set(&mut self, key: String, value: String) {
-        self.map.insert(key, value);
-    }
-
-    //get gets the value for a key
-    pub fn get(&self, key: String) -> Option<String> {
-        let data = self.map.get(&key);
-        match data {
-            Some(value) => Some(value.to_string()),
-            None => None,
-        }
-    }
-
-    //remove remove a key and a value from the in-memory store
-    pub fn remove(&mut self, key: String) {
-        self.map.remove(&key);
-    }
-
-    //    pub fn open(&self, _path: &Path) -> Result<KvStore> {
-    //        unimplemented!();
-    //    }
+struct BufReaderWithPos<R: Read + Seek> {
+  reader: BufReader<R>,
+  pos: u64
 }
+
+//impl KvStore {
+//    #[warn(dead_code)]
+//    // creates a new instance of KvStore
+//    pub fn new() -> Self {
+//        KvStore {
+//            map: HashMap::new(),
+//        }
+//    }
+//
+//    //set creates a new Key Value pair
+//    pub fn set(&mut self, key: String, value: String) {
+//        self.map.insert(key, value);
+//    }
+//
+//    //get gets the value for a key
+//    pub fn get(&self, key: String) -> Option<String> {
+//        let data = self.map.get(&key);
+//        match data {
+//            Some(value) => Some(value.to_string()),
+//            None => None,
+//        }
+//    }
+//
+//    //remove remove a key and a value from the in-memory store
+//    pub fn remove(&mut self, key: String) {
+//        self.map.remove(&key);
+//    }
+//
+//    //    pub fn open(&self, _path: &Path) -> Result<KvStore> {
+//    //        unimplemented!();
+//    //    }
+//}
